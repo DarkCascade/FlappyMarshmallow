939fcacffb9c442e19d8b40ae5e50a108af6384f
James Donnell <darkcascade@gmail.com>
2015-11-16 12:02:43 -0700

    followed tut from "Develop Games in Scala with LibGDX"

--- FlappyMarshmallow/gdx-game/src/com/darkcascade/flappymarshmallow/MyGdxGame.java
+++ FlappyMarshmallow/gdx-game/src/com/darkcascade/flappymarshmallow/MyGdxGame.java
@@ -9,21 +9,40 @@ public class MyGdxGame implements ApplicationListener
 	Texture texture;
 	SpriteBatch batch;
 
+	ShapeRenderer _shapeRender;
+	OrthographicCamera _camera;
+
 	@Override
 	public void create()
 	{
 		texture = new Texture(Gdx.files.internal("android.jpg"));
 		batch = new SpriteBatch();
+
+		_camera = new OrthographicCamera(Gdx.graphics.getWidth, Gdx.graphics.getHeight);
+		_camera.position.set(Gdx.graphics.getWidth/2, Gdx.graphics.getHeight/2, 0);
+		_camera.update();
+
+		_shapeRender = new ShapeRenderer();
+		_shapeRender.setProjectionMatrix(_camera.combined);
 	}
 
 	@Override
 	public void render()
 	{        
-	    Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glLineWidth(20);
+
 		batch.begin();
-		batch.draw(texture, Gdx.graphics.getWidth() / 4, 0, 
-				   Gdx.graphics.getWidth() / 2, Gdx.graphics.getWidth() / 2);
+
+		_shapeRender.begin(ShapeType.Line);
+		_shapeRender.setColor(0, 0, 0, 1);
+		_shapeRender.circle(160, 284, 100);
+		_shapeRender.circle(120, 314, 18);
+		_shapeRender.circle(200, 314, 18);
+		_shapeRender.polyline(Array(100, 260, 160, 220, 220, 260.0f));
+		_shapeRender.end();
+
 		batch.end();
 	}
 
